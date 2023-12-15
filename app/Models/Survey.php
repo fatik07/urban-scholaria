a9122e4a65044f9dcf47317dcdb79ea5b4e9240f
fatik07 <fatichur.r07@gmail.com>
2023-12-15 10:30:33 +0700

    fix: penambahan kolom dokumen_surat_tugas di survey

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-  use HasFactory;
+    use HasFactory;
 
-  public $table = 'survey';
+    public $table = 'survey';
 
-  protected $fillable = ['surat_id', 'user_id','nama_survey', 'deskripsi_survey', 'jadwal_survey','tenggat_survey', 'status', 'foto_survey', 'alamat_survey', 'longitude', 'latitude', 'dokumen_survey', 'alasan_ditolak'];
+    protected $fillable = ['surat_id', 'user_id', 'nama_survey', 'deskripsi_survey', 'jadwal_survey', 'tenggat_survey', 'status', 'foto_survey', 'alamat_survey', 'longitude', 'latitude', 'dokumen_surat_tugas', 'dokumen_survey', 'alasan_ditolak'];
 
-  public function surat()
-  {
-    return $this->belongsTo(Surat::class, 'surat_id');
-  }
+    public function surat()
+    {
+        return $this->belongsTo(Surat::class, 'surat_id');
+    }
 }
