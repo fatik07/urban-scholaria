f7ed5bb32555aaec3d04054aed0f3fdd6e4268e8
fatik07 <fatichur.r07@gmail.com>
2023-12-19 13:48:00 +0700

    fix: feedback untuk status ditolak juga

--- app/Http/Controllers/API/FeedbackController.php
+++ app/Http/Controllers/API/FeedbackController.php
@@ -10,52 +10,52 @@ use Illuminate\Support\Facades\Validator;
 
 class FeedbackController extends Controller
 {
-  public function index()
-  {
-    try {
-      // $feedbacks = Ulasan::with('surat')->get();
-      $feedbacks = Ulasan::with('surat')
-        ->join('surat', 'ulasan.surat_id', '=', 'surat.id')
-        ->join('user', 'surat.user_id', '=', 'user.id')
-        ->select('ulasan.*', 'surat.nama as nama_surat', 'user.nama_lengkap as nama_lengkap', 'user.nomor_identitas as nomor_identitas')
-        ->get();
-      return response()->json(['success' => true, 'message' => 'Data feedback berhasil diambil', 'data' => $feedbacks]);
-    } catch (\Exception $e) {
-      return response()->json(['success' => false, 'message' => $e->getMessage()]);
+    public function index()
+    {
+        try {
+            // $feedbacks = Ulasan::with('surat')->get();
+            $feedbacks = Ulasan::with('surat')
+                ->join('surat', 'ulasan.surat_id', '=', 'surat.id')
+                ->join('user', 'surat.user_id', '=', 'user.id')
+                ->select('ulasan.*', 'surat.nama as nama_surat', 'user.nama_lengkap as nama_lengkap', 'user.nomor_identitas as nomor_identitas')
+                ->get();
+            return response()->json(['success' => true, 'message' => 'Data feedback berhasil diambil', 'data' => $feedbacks]);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()]);
+        }
     }
-  }
 
-  public function create(Request $request)
-  {
-    try {
-      if (auth()->user()->role->nama !== 'Pemohon') {
-        return response()->json(['message' => 'Akses ditolak'], 403);
-      }
+    public function create(Request $request)
+    {
+        try {
+            if (auth()->user()->role->nama !== 'Pemohon') {
+                return response()->json(['message' => 'Akses ditolak'], 403);
+            }
 
-      $validator = Validator::make($request->all(), [
-        "surat_id" => "required|exists:surat,id",
-        "isi" => "required|string|max:255",
-      ]);
+            $validator = Validator::make($request->all(), [
+                "surat_id" => "required|exists:surat,id",
+                "isi" => "required|string|max:255",
+            ]);
 
-      if ($validator->fails()) {
-        return response()->json($validator->errors());
-      };
+            if ($validator->fails()) {
+                return response()->json($validator->errors());
+            };
 
-      $suratId = $request->surat_id;
-      $surat = Surat::findOrFail($suratId);
+            $suratId = $request->surat_id;
+            $surat = Surat::findOrFail($suratId);
 
-      if ($surat->status !== 'Selesai') {
-        return response()->json(['message' => 'Surat belum selesai'], 400);
-      }
+            if ($surat->status !== 'Selesai' && $surat->status !== 'Ditolak') {
+                return response()->json(['message' => 'Surat belum selesai ataupun ditolak'], 400);
+            }
 
-      Ulasan::create([
-        'surat_id' => $suratId,
-        'isi' => $request->isi,
-      ]);
+            Ulasan::create([
+                'surat_id' => $suratId,
+                'isi' => $request->isi,
+            ]);
 
-      return response()->json(['success' => true, 'message' => 'Feedback berhasil disimpan']);
-    } catch (\Exception $e) {
-      return response()->json(['success' => false, 'message' => $e->getMessage()]);
+            return response()->json(['success' => true, 'message' => 'Feedback berhasil disimpan']);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage()]);
+        }
     }
-  }
 }
